1fce34d68cc5d977cc389d50ed033d3b7d20f81e
Alvin Rizki <4lvin.rizki@gmail.com>
2023-03-19 21:46:47 +0700

    add grpc publisher command

--- cmd/grpc_producer.go
+++ cmd/grpc_producer.go
@@ -1,8 +1,21 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill"
+	wmkafka "github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/cikupin/learn-firehose/config"
+	"github.com/cikupin/learn-firehose/payload"
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/proto"
 )
 
 var grpcProducerCmd = &cobra.Command{
@@ -10,9 +23,68 @@ var grpcProducerCmd = &cobra.Command{
 	Short: "produce GRPC message to Kafka in every 5 seconds",
 	Run: func(cmd *cobra.Command, args []string) {
 		config.LoadEnv()
+		doGRPC()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(grpcProducerCmd)
+}
+
+func doGRPC() {
+	kafka0Host := fmt.Sprintf("%s:%d", config.Cfg.Kafka.Host, config.Cfg.Kafka.Port)
+	pubConfig := wmkafka.PublisherConfig{
+		Brokers:               []string{kafka0Host},
+		Marshaler:             wmkafka.DefaultMarshaler{},
+		OverwriteSaramaConfig: wmkafka.DefaultSaramaSyncPublisherConfig(),
+	}
+
+	publisher, err := wmkafka.NewPublisher(pubConfig, watermill.NewStdLogger(true, false))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	go func() {
+		ticker := time.NewTicker(5 * time.Second)
+		done := make(chan bool)
+		go func() {
+			for {
+				select {
+				case <-done:
+					return
+				case t := <-ticker.C:
+					publishGRPC(t, publisher)
+				}
+			}
+		}()
+	}()
+
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	<-s
+
+	publisher.Close()
+	fmt.Println("application stopped...")
+}
+
+func publishGRPC(t time.Time, publisher *wmkafka.Publisher) {
+	newID, _ := uuid.NewRandom()
+	newPayload := &payload.Flag{
+		Id: newID.String(),
+	}
+
+	bytePayload, err := proto.Marshal(newPayload)
+	if err != nil {
+		log.Printf("[ERROR - marshal proto] %s\n", err.Error())
+		return
+	}
+
+	newMessage := message.NewMessage(newID.String(), bytePayload)
+	err = publisher.Publish(config.Cfg.Kafka.TopicGRPC, newMessage)
+	if err != nil {
+		log.Printf("[ERROR - publish message] %s\n", err.Error())
+		return
+	}
+
+	fmt.Printf("---> [%v] Publishing :%s\n", t.Format(time.Kitchen), newID.String())
 }
--- config/config_object.go
+++ config/config_object.go
@@ -3,8 +3,10 @@ package config
 var Cfg config
 
 type kafkaConfig struct {
-	Host string `env:"KAFKA_HOST"`
-	Port int    `env:"KAFKA_PORT"`
+	Host      string `env:"KAFKA_HOST"`
+	Port      int    `env:"KAFKA_PORT"`
+	TopicGRPC string `env:"KAFKA_TOPIC_GRPC"`
+	TopicJSON string `env:"KAFKA_TOPIC_JSON"`
 }
 
 type config struct {
